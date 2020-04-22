4c5f88e9218cc93103466df21dd5e1bc762798ee
pedri <pedri@LAPTOP-9RVR1IK2>
2020-04-22 22:56:15 +0200

    codigo de bloques

--- Sanalgonandro/src/Bloque.h
+++ Sanalgonandro/src/Bloque.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "Vector2D.h"
+
+
+class Bloque {
+private:
+	float lado;
+	Vector2D posicion;
+	Vector2D velocidad;
+	Vector2D aceleracion;
+public:
+	Bloque();
+	virtual ~Bloque();
+	void Dibuja();
+	void Mueve(float t);
+	void SetPos(float ix, float iy);
+	void SetVel(float vx, float vy);
+};
