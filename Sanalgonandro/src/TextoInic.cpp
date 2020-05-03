3e93f683a27df374db8e0b218deac313a423852f
pedri <pedri@LAPTOP-9RVR1IK2>
2020-05-04 01:05:26 +0200

    texto inicial de nivel, queda aun

---
+++ Sanalgonandro/src/TextoInic.cpp
@@ -0,0 +1,26 @@
+#include "TextoInic.h"
+#include <stdio.h>
+
+TextoInic::TextoInic(float x1, float y1, float x2, float y2) {
+	limite1.x = x1;
+	limite2.x = x2;
+	limite1.y = y1;
+	limite2.y = y2;
+	rojo = 0;
+	verde = 250;
+	azul = 0;
+}
+void TextoInic::Dibuja() {
+	glDisable(GL_LIGHTING);
+	glBegin(GL_POLYGON);
+	glColor3ub(rojo, verde, azul);
+	glVertex3d(limite1.x, limite1.y, 0);
+	glVertex3d(limite2.x, limite1.y, 0);
+	glVertex3d(limite2.x, limite2.y, 0);
+	glVertex3d(limite1.x, limite2.y, 0);
+	glEnd();
+	glEnable(GL_LIGHTING);
+}
+TextoInic::~TextoInic() {
+
+}
---
+++ Sanalgonandro/src/TextoInic.h
@@ -0,0 +1,20 @@
+#pragma once
+#pragma once
+#include "Vector2D.h"
+#include <glut.h>
+
+
+class TextoInic {
+public:
+	TextoInic(float x1, float y1, float x2, float y2);
+	virtual ~TextoInic();
+	void Dibuja();
+private:
+	Vector2D limite1;
+	Vector2D limite2;
+	unsigned char rojo;
+	unsigned char verde;
+	unsigned char azul;
+
+};
+
