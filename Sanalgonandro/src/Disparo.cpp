e54f0421d1ba5af03618be3c6a3b5d99905a2a75
molina.angel17 <molina.angel17@gmail.com>
2020-05-04 12:30:53 +0200

    disparo

--- Sanalgonandro/src/Disparo.cpp
+++ Sanalgonandro/src/Disparo.cpp
@@ -3,14 +3,24 @@
 
 Disparo::Disparo() {
 	radio = 0.25f;
-	velocidad.y = 0.5f;
+	velocidad.x = 200.0f;
+	posicion = origen;
 }
+
+Disparo::Disparo(Vector2D Orig) {
+	radio = 0.25f;
+	origen = Orig;
+	velocidad.x = 200.0f;
+	posicion = origen;
+}
+
+
 Disparo::~Disparo() {
 
 }
 
 void Disparo::Dibuja() {
-	glColor3f(0.0f, 1.0f, 1.0f);
+	/*glColor3f(0.0f, 1.0f, 1.0f);
 	glPushMatrix();
 	glTranslatef(posicion.x, posicion.y, 0);
 	glutSolidSphere(radio, 20, 20);
@@ -34,18 +44,41 @@ void Disparo::Dibuja() {
 		glVertex3d(origen - 0.125, 0, 0);
 	}
 	glEnd();
+	glEnable(GL_LIGHTING);*/
+
+
+	glColor3f(0.0f, 1.0f, 1.0f);
+	glPushMatrix();
+	glTranslatef(posicion.x, posicion.y, 0);
+	glutSolidSphere(radio, 20, 20);
+	glTranslatef(-posicion.x, -posicion.y, 0);
+
+	glDisable(GL_LIGHTING);
+	glBegin(GL_LINES);
+
+	glVertex3d(origen.x, origen.y, 0.0f);
+	glVertex3d(posicion.x, posicion.y, 0.0f);
+	glEnd();
 	glEnable(GL_LIGHTING);
+
+
+	glPopMatrix();
 }
 
 void Disparo::Mueve(float t) {
-	posicion = posicion + velocidad * t + aceleracion * (0.5f*t*t);
+	posicion = posicion + velocidad * t + aceleracion * (0.5f * t * t);
 	velocidad = velocidad + aceleracion * t;
-	//pelotudo
+
 }
 
 void Disparo::SetPos(float ix, float iy) {
 	posicion.x = ix;
 	posicion.y = iy;
 
-	origen = posicion.x;
 }
+
+void Disparo::SetOrig(float ix, float iy) {
+	origen.x = ix;
+	origen.y = iy;
+}
+
--- Sanalgonandro/src/Disparo.h
+++ Sanalgonandro/src/Disparo.h
@@ -4,14 +4,19 @@
 class Disparo {
 private:
 	float radio;
-	float origen;
+	Vector2D origen;
 	Vector2D posicion;
 	Vector2D velocidad;
 	Vector2D aceleracion;
 public:
 	Disparo();
+	Disparo(Vector2D Orig);
 	virtual ~Disparo();
 	void Dibuja();
 	void Mueve(float t);
 	void SetPos(float ix, float iy);
+	void SetOrig(float ix, float iy);
+	Vector2D GetPos() { return posicion; }
+	float GetRadio() { return radio; }
+	void SetVel(float vx, float vy) { velocidad.x = vx, velocidad.y = vy; }
 };
