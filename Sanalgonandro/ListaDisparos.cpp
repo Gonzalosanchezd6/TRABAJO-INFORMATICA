d70fe091b3f01b3359cfadbdded8de36272be92c
molina.angel17 <molina.angel17@gmail.com>
2020-05-04 12:29:23 +0200

    lista disparos

---
+++ Sanalgonandro/ListaDisparos.cpp
@@ -0,0 +1,94 @@
+#include"ListaDisparos.h"
+#include"Interaccion.h"
+
+ListaDisparos::ListaDisparos() {
+	numero = 0;
+	for (int i = 0; i < MAX_DISPAROS; i++) {//poner los punteros a null
+		lista[i] = 0;
+	}
+}
+
+ListaDisparos::~ListaDisparos() {
+
+}
+
+bool ListaDisparos::agregar(Disparo* d) {
+	for (int i = 0; i < numero; i++) {// si ya existe que no se vuelva a añadir
+		if (lista[i] == d) {
+			return false;
+		}
+	}
+	if (numero < MAX_DISPAROS) {
+		lista[numero++] = d;
+		return true;
+	}
+	else {
+		return false;// no se ha podido agregar una esfera
+	}
+}
+
+void ListaDisparos::destruirContenido() {
+	for (int i = 0; i < numero; i++) {
+		delete lista[i];
+		numero = 0;
+	}
+}
+
+void ListaDisparos::mueve(float t) {
+	for (int i = 0; i < numero; i++) {
+		lista[i]->Mueve(t);
+	}
+}
+
+void ListaDisparos::dibuja() {
+	for (int i = 0; i < numero; i++) {
+		lista[i]->Dibuja();
+		Vector2D pos = lista[i]->GetPos();//si se pasa de largo se elimina
+		if (pos.x > 40) {
+			Eliminar(i);
+		}
+	}
+
+}
+
+Disparo* ListaDisparos::colision(Pared p) {
+	for (int i = 0; i < numero; i++) {
+		if (Interaccion::colision(*lista[i], p)) {
+			lista[i]->SetVel(0.0f, 0.0f);
+			return lista[i];
+		}
+	}
+	return 0;
+}
+
+Disparo* ListaDisparos::colision(Enemigo1 e) {
+	for (int i = 0; i < numero; i++) {
+		if (Interaccion::colision(*lista[i], e)) {
+			lista[i]->SetVel(0.0f, 0.0f);
+			return lista[i];
+		}
+	}
+	return 0;
+}
+
+
+
+void ListaDisparos::Eliminar(int index) {
+	if ((index < 0) || (index >= numero)) {
+		return;
+	}
+	delete lista[index];
+	numero--;
+	for (int i = index; i < numero; i++) {
+		lista[i] = lista[i + 1];
+	}
+}
+
+void ListaDisparos::Eliminar(Disparo* d) {
+	for (int i = 0; i < numero; i++) {
+		if (lista[i] == d) {
+			Eliminar(i);
+			return;
+		}
+	}
+}
---
+++ Sanalgonandro/ListaDisparos.h
@@ -0,0 +1,23 @@
+#pragma once
+#define MAX_DISPAROS 10
+#include "Disparo.h"
+#include"Pared.h"
+#include"EnemigoLv1.h"
+
+class ListaDisparos {
+public:
+	ListaDisparos();
+	virtual ~ListaDisparos();
+	bool agregar(Disparo* d);
+	void destruirContenido();
+	void Eliminar(Disparo* d);
+	void Eliminar(int index);
+	void mueve(float t);
+	void dibuja();
+	Disparo* colision(Pared p);
+	Disparo* colision(Enemigo1 e);
+
+private:
+	Disparo* lista[MAX_DISPAROS];
+	int numero;
+};
